d627f3a02b0014043234c6dbbe4cb169c19553d0
Teng YANG <teng.yang@oocl.com>
2019-07-25 12:52:11 +0800

    reverse datetime validate

--- src/services/base.js
+++ src/services/base.js
@@ -1,20 +1,21 @@
 import axios from 'axios'
-import {notification} from 'ant-design-vue'
+import { notification } from 'ant-design-vue'
 
 const request = axios.create({})
 
 request.interceptors.response.use(
-    res => {
-        return res.data
-    },
-    error => {
-        if(error.response) {
-            notification.error({
-                message: error.response.error,
-                description: error.response.message
-            })
-        }
+  res => {
+    return res.data
+  },
+  error => {
+    if (error.response) {
+      notification.error({
+        message: error.response.data.error,
+        description: error.response.data.message
+      })
     }
+    throw error
+  }
 )
 
 export default request
--- src/views/Reverse.vue
+++ src/views/Reverse.vue
@@ -1,6 +1,7 @@
 <script>
 import { message } from "ant-design-vue";
 import { reverseReceipt } from "../services/packageService";
+import moment from "moment";
 const formItemLayout = {
   labelCol: {
     span: 5
@@ -11,11 +12,22 @@ const formItemLayout = {
 };
 
 export default {
-  name: "reverse",
+  name: "Reverse",
   data() {
+    const timeValidator = (rule, value, cb) => {
+      if (!value) {
+        cb(new Error("请选择日期"));
+      }
+      const hour = moment(value).hour();
+      if (hour >= 20 || hour < 8) {
+        cb(new Error("超出时间范围"));
+      }
+      cb();
+    };
     return {
-      form: this.$form.createForm(this),
-      formItemLayout
+      formItemLayout,
+      timeValidator,
+      form: this.$form.createForm(this)
     };
   },
   methods: {
@@ -46,13 +58,22 @@ export default {
           v-decorator="['no',{rules: [{ required: true, message: 'Please input your no!' }]}]"
         />
       </a-form-item>
-      <a-form-item label="取件时间" :label-col="{span: 5}" :wrapper-col="{ span: 12 }">
+      <a-form-item label="取件时间" v-bind="formItemLayout">
         <a-date-picker
-          v-decorator="['reserveTime', {rules: [{ required: true, message: 'Please input your time!' }]}]"
+          :showTime="true"
+          format="YYYY-MM-DD HH:mm:ss"
+          v-decorator="['reserveTime', {rules:[{required: true, message: '请选择时间'}, {validator: timeValidator}]}]"
         />
       </a-form-item>
-      <a-button type="primary" html-type="submit">预约</a-button>
-      <a-button @click="resetForm">取消</a-button>
+      <a-form-item
+        :wrapper-col="{
+        xs: { span: 24, offset: 0 },
+        sm: { span: 16, offset: 5 },
+      }"
+      >
+        <a-button type="primary" html-type="submit">预约</a-button>
+        <a-button @click="resetForm">取消</a-button>
+      </a-form-item>
     </a-form>
   </div>
 </template>
